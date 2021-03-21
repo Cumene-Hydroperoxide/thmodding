4090219d20d713efe25902905bed62e1fafe60fb
Cumene-Hydroperoxide <76613902+Cumene-Hydroperoxide@users.noreply.github.com>
2021-03-21 11:12:47 +0900

    Add files via upload

---
+++ kimchiden/patch.js
@@ -0,0 +1,19 @@
+{
+    "id": "kimchiden",
+    "dependencies": [
+        "nmlgc/base_tsa",
+        "nmlgc/base_tasofro",
+        "nmlgc/script_latin"
+    ],
+    "servers": [
+        "https://raw.githubusercontent.com/Cumene-Hydroperoxide/thmodding/master/"
+    ],
+    "fonts": {
+        "NanumGothic.ttf": true,
+        "NanumMyeongjo.ttf": true,
+        "a자막체.ttf": true,
+        "헤움예향162.ttf": true,
+        "MDGaesung.ttf": true
+    },
+	"title": "Korean version of Kanjuden"
+}
