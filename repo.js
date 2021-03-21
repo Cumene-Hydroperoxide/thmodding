5b62f4617e9a971b64042ec9b97dd85ccc0cd5de
Cumene-Hydroperoxide <76613902+Cumene-Hydroperoxide@users.noreply.github.com>
2021-03-21 11:11:05 +0900

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "swl20040421@gmail.com",
+	"id": "cumene",
+	"patches": {
+		"kimchiden": "Korean version Touhou Kanjuden",
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Cumene-Hydroperoxide/thmodding/master/"
+	],
+	"title": "Cumene's patches"
+}
